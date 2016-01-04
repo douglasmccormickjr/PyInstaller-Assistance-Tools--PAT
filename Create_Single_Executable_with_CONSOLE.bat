af0105ac72e01ab888363e25a71576ec4a3a6ec8
Douglas McCormick Jr <douglasmccormickjr@users.noreply.github.com>
2016-01-03 23:31:59 -0500

    Create_Single_Executable_with_CONSOLE
    This is the batch script that will compile your python application (via PyInstaller) into:
      - a single executable file (on Windows)
      - WITH A CONSOLE (so it suggests that you don't have a GUI interface -- aka -- the running app will launch a Windows console window and remain there until it terminates)

---
+++ Create_Single_Executable_with_CONSOLE.bat
@@ -0,0 +1,38 @@
+@echo off
+echo %~n1
+echo %~dp1
+set filename=%~n1
+set filepath=%~dp1
+set distname=ApplicationDistribution64bit\
+set filedist=%filepath%%distname%
+set filefina=%filedist%%filename%\
+echo %filename%
+echo %filepath%
+echo %filedist%
+echo %distname%
+echo %filefina%
+
+IF EXIST "%filedist%" goto KeepGoing
+IF Not EXIST "%filedist%" goto CreateDirectory
+
+:CreateDirectory
+echo ++++ Creating NEW application distribution folder...
+mkdir "%filedist%"
+goto KeepGoing
+
+:KeepGoing
+cd "%filedist%"
+echo ++++ Creating NEW application sub/version folder...
+mkdir "%filename%"
+cd "%filename%"
+echo ++++ Creating .spec file now...
+"pyi-makespec.exe" --onefile --console "%1"
+echo ++++ Creating FIXED spec file...
+REM ping 192.0.2.2 -n 1 -w 10000 > nul
+"pyi-fixspec.exe" %filefina%%filename%.spec "%1"
+echo ++++ Creating final build file...
+pyi-build.exe %filefina%%filename%.spec
+echo ++++ Opening distribution output window
+explorer /n, %filefina%
+echo ++++ exiting now...
+exit /b 0
